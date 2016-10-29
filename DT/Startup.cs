92ab6cecb1ac6d5bfcd50cc4626bc95c48a29d70
Igor Didenko <ded914@gmail.com>
2016-10-29 11:31:45 -0400

    Initial commint

---
+++ DT/Startup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DT.Startup))]
+namespace DT {
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
